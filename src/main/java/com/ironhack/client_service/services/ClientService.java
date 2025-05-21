bfaaa48b1266b08c50f05e51c77001e835c7fcbe
Marshall-Bits <kz_@hotmail.es>
2025-05-21 12:06:14 +0200

    add findclientById

---
+++ src/main/java/com/ironhack/client_service/services/ClientService.java
@@ -0,0 +1,29 @@
+package com.ironhack.client_service.services;
+
+import com.ironhack.client_service.models.Client;
+import com.ironhack.client_service.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired // inyección por campo (básica)
+    ClientRepository clientRepository;
+
+    // recibir un usuario por ID
+    ResponseEntity<?> findClientById(Long id) {
+        Optional<Client> foundClient = clientRepository.findById(id);
+
+        if (foundClient.isPresent()) {
+            return new ResponseEntity<>(foundClient, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Cliente no encontrado", HttpStatus.NOT_FOUND);
+        }
+
+    }
+}
